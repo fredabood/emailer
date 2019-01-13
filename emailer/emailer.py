2e9b7b2e33bc444fc00b162ca05724e28fece137
Fred Abood <fred@fredabood.com>
2019-01-13 15:12:23 -0600

    committing installed emailer

--- emailer/emailer.py
+++ emailer/emailer.py
@@ -1,19 +1,56 @@
 import os
+
 import smtplib
 
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
 
-def send_email(recipient, subject, body, sender=os.environ.get('EMAIL_ADDRESS'), password=os.environ.get('EMAIL_PASSWORD')):
 
-    message = f"From: {sender}\nTo: {recipient}\nSubject: {subject}\n\n{body}"
+class Email():
 
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(sender, password)
-        server.sendmail(sender, recipient, message)
-        server.close()
-        print("Successfully sent email")
+    def __init__(self, recipient, subject, body, files=[], sender=os.environ.get('EMAIL_ADDRESS'), password=os.environ.get('EMAIL_PASSWORD')):
 
-    except Exception as e:
-        print("Failed to send email because of: " + e)
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+        self.sender = sender
+        self.password = password
+        self.files = files
+        self.message = self.build_msg()
+
+    def send(self):
+
+        try:
+            server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+            server.ehlo()
+            server.starttls()
+            server.login(self.sender, self.password)
+            server.sendmail(self.sender, self.recipient, self.message.as_string())
+            server.close()
+            print("Successfully sent email")
+
+        except Exception as e:
+            print("Failed to send email because of: " + e)
+
+    def build_msg(self):
+        msg = MIMEMultipart()
+        msg['From'] = self.sender
+        msg['To'] = ', '.join(self.recipient) if type(self.recipient) is list else ', '.join([self.recipient])
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = self.subject
+
+        msg.attach(MIMEText(self.body))
+
+        for file in self.files:
+            # part = MIMEApplication(attachment.read(), Name=basename(file))
+            filename = file.split('/')[-1]
+            attachment = open(file, "rb").read()
+
+            part = MIMEApplication(attachment, Name=filename)
+            part['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+            msg.attach(part)
+
+        return msg
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(
-    name='emailer',
+    name='Emailer',
     version='0.1',
     url='https://www.github.com/fredabood/emailer',
     author='Fred Abood',
